d7f5c222fa17addb4fd102383241786ec7eed0cf
cam rains <csr66@nau.edu>
2019-03-20 15:51:37 -0700

    app2 finished

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,18 +3,18 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: ["meats", "cheeses", "ice creams", "flurbos", "gorgon sh*t"]
+    };
+  }
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    let listOnScreen = this.state.list.map((element, index) => {
+      return <h1 key={index}> {element} </h1>;
+    });
+
+    return <div className="App">{listOnScreen} </div>;
   }
 }
 
